669bf7431066df9ac45476ef1fda1e776e82f9cb
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2018-10-15 16:11:18 -0500

    initial

---
+++ Board.java
@@ -0,0 +1,48 @@
+public class Board{
+    // private int vPos;
+    // private int hPos;
+    // private final int PIECES_PER_PLAYER = 12;
+    //private String boardPosition;
+    //private String pieceName;
+    // private final int BOARD_SQUARES = 8;
+    // private int b = 0;
+    //private Boolean isFilled = false;
+    private Piece[][] board = new Piece[8][8];
+
+    public void setPiece(Piece a, int x, int y){
+        // error if x or y are > 7
+        board[x][y] = p;
+    }
+
+    public Boolean checkOccupied(int x, int y){
+        return (board[x][y] != null);
+    }
+
+    public Color checkColor(int x, int y){
+        return board[x][y].getColor();
+    }
+
+    // public Board(int x, int y){
+    //     vPos = x;
+    //     hPos = y;
+    // }
+
+    // public void boardSize(int h, int v){
+    //     chessBoard = new String[h][v];
+    // }
+    // public void fillEmptyBoard(){
+    //     for (int x = 0; x < 8; x++) {
+    //         for (int y = 0; y < 8; y++) {
+    //             Board[x][y] = 0;
+    //         }
+    //     }
+    // }
+    
+    // public String getPiece(){
+    //     String boardPosition = (hPos + "" + vPos);
+    //     return boardPosition;
+    // }
+    // public void printBoard(int x, int y){
+    //     System.out.print(" " + Board[x][y].toString());
+    // }
+}
---
+++ CheckersPiece.java
@@ -0,0 +1,58 @@
+public class CheckersPiece{
+    private static int numOfPieces;
+    private Color color;
+    private int id;
+
+    public CheckersPiece(){
+        id = numOfPieces
+        numOfPieces++
+    }
+    
+    public void setColor(Color c){
+        color = c;
+    }
+    
+    public Color getColor(){
+        return color;
+    }
+    
+    
+    // private Color color;
+    // // private int vPos;
+    // // private int hPos;
+    // // private String pieceType;
+    // private int[][] checkersPiece = new int[8][8];
+
+    // public checkesPiece(Color c, int x, int y){
+    //     color = c;
+    // }
+    // public void setColor(String c){
+    //     Color = c;
+    // }
+    // public String getColor(){
+    //     Return Color;
+    // }
+
+    // public int getPosition(){
+    //     return vPos + " " + hPos;
+    // }
+    // public void setPosition(int x, int y){
+    //     vPos = x;
+    //     hPos = y;
+    // }
+    // public String getColor(){
+    //     return color;
+    // }
+    // public void setColor(String pieceColor){
+    //     color = pieceColor;
+    // }
+    // public String getPieceType(){
+    //     return pieceType;
+    // }
+    // public void setPieceType(String name){
+    //     pieceType = name;
+    // }
+    // public String printInfo(){
+    //     return getPieceType() + " is " + getColor() + " at " getPosition();
+    // }
+}
